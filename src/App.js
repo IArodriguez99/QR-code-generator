68257bcf4fc6e5198fc7b66537f35ade36c61406
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2022-02-18 15:08:20 -0600

    onClick function for generating qr code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import './App.css';
+import React, {useState} from 'react'
+import QRCode from 'qrcode'
+
+
+
 
 
 function App() {
+
+  const [text, setText] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+
+  const generateQrCode = async () => {
+    try {
+          const response = await QRCode.toDataURL(text);
+          setImageUrl(response);
+    }catch (error) {
+      console.log(error);
+    }
+  }
+
+  
   return (
     <div className="App">
       <div class="container grid grid-col-3 grid-flow-row min-h-screen">
           
           <div class="grid col-span-3 place-items-center">Generate and scan QR codes</div>
-                   
+
           <div class="grid row-span-6 justify-items-center items-start">
             <div class="flex items-center border-b border-teal-500 py-2 my-8">
-            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Enter a URL" aria-label="Full name" />
-              <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Enter a URL" aria-label="Full name" onChange={(e) => setText(e.target.value)}/>
+              <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button" onClick={() => generateQrCode()}>
                 Generate
               </button>
             </div>
           </div>
           
-          <div class="grid row-span-6 justify-items-center items-start">
+          <div class="grid row-span-6 justify-items-center items-start max-w-fit">
               <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 my-11 rounded" type="button">
-                Generate
+                Scan QR Code
               </button>
             
           </div>
