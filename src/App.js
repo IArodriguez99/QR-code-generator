22f9457552a1586130d9f1156ad4e82b0373ab93
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2022-02-21 21:00:09 -0600

    Image url state

--- src/App.js
+++ src/App.js
@@ -28,17 +28,25 @@ function App() {
           
           <div class="grid col-span-3 place-items-center">Generate and scan QR codes</div>
 
-          <div class="grid row-span-6 justify-items-center items-start">
+          <div class="grid row-span-5 justify-items-center items-start">
             <div class="flex items-center border-b border-teal-500 py-2 my-8">
             <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Enter a URL" aria-label="Full name" onChange={(e) => setText(e.target.value)}/>
               <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button" onClick={() => generateQrCode()}>
                 Generate
               </button>
             </div>
+            
+            <div>
+              {imageUrl ? (
+                <a href={imageUrl} download>
+                  <img src={imageUrl} alt="img"/>
+                </a>) : null}
+            </div>
+            
           </div>
           
           <div class="grid row-span-6 justify-items-center items-start max-w-fit">
-              <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 my-11 rounded" type="button">
+              <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 my-10 rounded" type="button">
                 Scan QR Code
               </button>
             
